645507bdcaf349d70aea3d785c95cc4a090d21bd
João Leonardo Rodrigues Santos <jotaleo.ce3@gmail.com>
2026-01-12 11:27:43 -0300

    Refactor production method to handle components

--- mrp/services/produto_service.py
+++ mrp/services/produto_service.py
@@ -4,15 +4,15 @@ from models.insumo import Insumo
 
 class ProdutoService():
    
-   def realizar_producao(self, produto_final, qtd_desejada, estoque_service):
-      if not isinstance(produto_final, ProdutoFinal):
-         raise TypeError("Este item não é um Produto Final")
+   def realizar_producao(self, produto, qtd_desejada, estoque_service):
+      if not isinstance(produto, ProdutoFinal) and not (isinstance(produto, Insumo) and produto.eh_composto):
+         raise TypeError("Este produto não pode ser produzido")
 
-      if not produto_final.estrutura:
+      if not produto.estrutura:
          raise ValueError("Este produto não tem estrutura cadastrada")
 
       pode_produzir = True
-      for item in produto_final.estrutura:
+      for item in produto.estrutura:
          necessario = item.quantidade * qtd_desejada
          
          produto_insumo = item.insumo
@@ -21,17 +21,17 @@ class ProdutoService():
             disponivel = estoque_service.empresa.estoque.itens[produto_insumo.codigo]["quantidade"]
          
          if disponivel < necessario:
-            print(f"Falta Insumo: {item.insumo.nome} (Precisa: {necessario}, Tem: {disponivel})")
+            print(f"Falta Componente: {item.insumo.nome} (Precisa: {necessario}, Tem: {disponivel})")
             pode_produzir = False
 
       if pode_produzir:
-         for item in produto_final.estrutura:
+         for item in produto.estrutura:
             estoque_service.remover(item.insumo.codigo, item.quantidade * qtd_desejada)
          
-         estoque_service.adicionar(produto_final.codigo, qtd_desejada)
+         estoque_service.adicionar(produto.codigo, qtd_desejada)
          
-         print(f"Sucesso: {qtd_desejada} unidade(s) de '{produto_final.nome}' produzidas!")
-         return produto_final
+         print(f"Sucesso: {qtd_desejada} unidade(s) de '{produto.nome}' produzidas!")
+         return produto
       
       print("Produção cancelada: estoque insuficiente")
       return None
@@ -45,3 +45,6 @@ class ProdutoService():
             raise ValueError("Este insumo já está na estrutura do produto")
 
       produto.estrutura.append(ItemEstrutura(insumo, quantidade))
+      
+      if isinstance(produto, Insumo):
+         produto.eh_composto = True
