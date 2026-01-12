a84eb0d30a786c6a1c7952cf37bfcfb4da2a229a
João Leonardo Rodrigues Santos <jotaleo.ce3@gmail.com>
2026-01-12 11:15:01 -0300

    Add option to consult product in main menu

--- mrp/ui/menu.py
+++ mrp/ui/menu.py
@@ -7,6 +7,7 @@ from ui.interfaces.criar_estrutura import criar_estrutura_ui
 from ui.interfaces.produzir_produto import produzir_produto_ui
 from ui.interfaces.gerenciar_estoque import gerenciar_estoque_ui
 from ui.interfaces.listar_produtos import listar_produtos_ui
+from ui.interfaces.consultar_produto import consultar_produto_ui
 
 
 def menu_principal(empresa):
@@ -19,29 +20,33 @@ def menu_principal(empresa):
       print("3 - Produzir Produto")
       print("4 - Gerenciar Estoque")
       print("5 - Listar Produtos Cadastrados")
+      print("6 - Consultar Produto")
       print("0 - Sair")
 
       opcao = input("Escolha: ")
 
       match opcao:
-         case "1": #OK
+         case "1":
             cadastrar_produto_ui(empresa)
 
-         case "2": #OK
+         case "2":
             criar_estrutura_ui(empresa)
 
          case "3":
             produzir_produto_ui(empresa)
 
-         case "4": #OK
-            gerenciar_estoque_ui(empresa) 
+         case "4":
+            gerenciar_estoque_ui(empresa)
 
          case "5":
            listar_produtos_ui(empresa)
+
+         case "6":
+            consultar_produto_ui(empresa)
 
          case "0":
             print("\nEncerrando sistema... Até logo!")
             break
 
          case _:
-            print("\nOpção inválida! Tente um número entre 0 e 5.")
+            print("\nOpção inválida! Tente um número entre 0 e 6.")
