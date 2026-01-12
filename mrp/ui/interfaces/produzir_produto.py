d04f84120bd621011f46bf09bd0d26fd96475184
João Leonardo Rodrigues Santos <jotaleo.ce3@gmail.com>
2026-01-12 11:08:46 -0300

    Integrate EstoqueService into produzir_produto_ui

--- mrp/ui/interfaces/produzir_produto.py
+++ mrp/ui/interfaces/produzir_produto.py
@@ -1,24 +1,27 @@
 from services.empresa_service import EmpresaService
 from services.produto_service import ProdutoService
+from services.estoque_service import EstoqueService
 
 def produzir_produto_ui(empresa):
    empresa_service = EmpresaService(empresa)
    produto_service = ProdutoService()
-
+   estoque_service = EstoqueService(empresa)
+   
    print("\n--- ORDEM DE PRODUÇÃO ---")
 
    codigo = input("Código do Produto Final: ")
 
    produto = empresa_service.buscar_produto(codigo)
    if not produto:
-      raise "Produto nao encontrado"
+      print("Produto nao encontrado")
+      return
 
    quantidade = int(input("Quantidade a produzir: "))
    if quantidade <= 0:
-      raise "Quantidade deve ser um número inteiro positivo."
+      print("Quantidade deve ser um número inteiro positivo.")
+      return
 
-   produto = produto_service.realizar_producao(produto, quantidade)
+   produto = produto_service.realizar_producao(produto, quantidade, estoque_service)
 
-   print(f"Produto '{produto.nome}' produzido com sucesso!!!")
-
-
+   if produto:
+      print(f"Produto '{produto.nome}' produzido com sucesso!!!")
