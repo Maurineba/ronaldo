f6daf67e8a5fcc399fa6d3afe1a63cf4fe8679d6
João Leonardo Rodrigues Santos <jotaleo.ce3@gmail.com>
2026-01-12 11:05:46 -0300

    Update produto_service.py

--- mrp/services/produto_service.py
+++ mrp/services/produto_service.py
@@ -3,10 +3,10 @@ from models.item_estrutura import ItemEstrutura
 from models.insumo import Insumo
 
 class ProdutoService():
-   def realizar_producao(self, produto_final, qtd_desejada, estoque):
+   
+   def realizar_producao(self, produto_final, qtd_desejada, estoque_service):
       if not isinstance(produto_final, ProdutoFinal):
          raise TypeError("Este item não é um Produto Final")
-
 
       if not produto_final.estrutura:
          raise ValueError("Este produto não tem estrutura cadastrada")
@@ -14,25 +14,34 @@ class ProdutoService():
       pode_produzir = True
       for item in produto_final.estrutura:
          necessario = item.quantidade * qtd_desejada
-         disponivel = estoque.obter_quantidade(item.insumo)
+         
+         produto_insumo = item.insumo
+         disponivel = 0
+         if produto_insumo.codigo in estoque_service.empresa.estoque.itens:
+            disponivel = estoque_service.empresa.estoque.itens[produto_insumo.codigo]["quantidade"]
+         
          if disponivel < necessario:
             print(f"Falta Insumo: {item.insumo.nome} (Precisa: {necessario}, Tem: {disponivel})")
             pode_produzir = False
 
       if pode_produzir:
          for item in produto_final.estrutura:
-            estoque.remover(item.insumo, item.quantidade * qtd_desejada)
-         estoque.adicionar(produto_final, qtd_desejada)
+            estoque_service.remover(item.insumo.codigo, item.quantidade * qtd_desejada)
+         
+         estoque_service.adicionar(produto_final.codigo, qtd_desejada)
+         
          print(f"Sucesso: {qtd_desejada} unidade(s) de '{produto_final.nome}' produzidas!")
+         return produto_final
+      
+      print("Produção cancelada: estoque insuficiente")
+      return None
 
-      return produto_final
-
-   def criar_estrutura(self, produto: ProdutoFinal, insumo: Insumo, quantidade: int):
+   def criar_estrutura(self, produto, insumo, quantidade: int):
       if quantidade <= 0:
          raise ValueError("Quantidade inválida")
 
       for item in produto.estrutura:
-         if item.insumo == insumo:
+         if item.insumo.codigo == insumo.codigo:
             raise ValueError("Este insumo já está na estrutura do produto")
 
       produto.estrutura.append(ItemEstrutura(insumo, quantidade))
